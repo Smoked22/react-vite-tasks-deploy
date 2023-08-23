5e035816046f5fce518ef68c1e9d92d669bb381d
Jonathan H <smokedhernandez@gmail.com>
2023-08-23 15:59:02 -0400

    Primer commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'mi primer tarea',
+        descripcion: 'mi primera tarea de react'
+    },
+    {
+        id: 1,
+        title: 'mi segunda tarea',
+        descripcion: 'mi segunda  tarea de react'
+    },
+    {
+        id: 2,
+        title: 'mi tercera tarea',
+        descripcion: 'mi tercera tarea de react'
+    }
+]
